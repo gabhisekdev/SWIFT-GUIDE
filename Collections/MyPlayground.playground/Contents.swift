b8541753b0c386acee79047575443cca5df466d7
Abhisek <abhisekbunty94@gmail.com>
2017-12-26 11:31:48 +0530

    Collection types initial commit

--- Collections/MyPlayground.playground/Contents.swift
+++ Collections/MyPlayground.playground/Contents.swift
@@ -2,12 +2,51 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
-//Swift provides 3 primary collection types -> arrays, sets, and dictionaries
 func exampleOf(exampleName: String, completion: ()->()) {
     print("Example of \(exampleName)")
     print("-------------------------")
     completion()
     print("\n")
+}
+
+
+//Swift provides 3 primary collection types -> arrays, sets, and dictionaries
+
+//MARK: Array
+exampleOf(exampleName: "Creating an empty array") {
+    let fruitList = [String]()
+    print("Fruit List -\(fruitList)")
+}
+
+exampleOf(exampleName: "Creating Array with values") {
+    let fruitList = ["Orange","Mango","Pineapple"]
+    print("Fruit List -\(fruitList)")
+}
+
+exampleOf(exampleName: "Creating an array - Mutable one") {
+    var fruitList = ["Orange","Mango","Pineapple"] //Just declare it as a variable and the rest addition and removing will follow soon
+    print("Fruit List -\(fruitList)")
+}
+
+exampleOf(exampleName: "Creating an array - Mutable one") {
+    var fruitList = ["Orange","Mango","Pineapple"] //Just declare it as a variable
+    print("Fruit List -\(fruitList)")
+}
+
+exampleOf(exampleName: "Concatenating Arrays") {
+    
+    let fruitList = ["Orange","Mango","Pineapple"]
+    print("Fruit List - \(fruitList)")
+    let vegetableList = ["Cabbage","Cucumber","Jackfruit"]
+    print("Vegetable List - \(fruitList)")
+    
+    let itemsList = fruitList + vegetableList
+    print("All items bought - \(itemsList)")
+    
+}
+
+exampleOf(exampleName: "Accessing and Modifying an Array") {
+    
+    let dataStructures = ["Stack","Queue","Linked List",""]
+    
 }

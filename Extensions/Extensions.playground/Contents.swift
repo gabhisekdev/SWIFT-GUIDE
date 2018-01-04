20f5eb156022a2b79e4271e1e94cbea19f350c6c
Abhisek <abhisekbunty94@gmail.com>
2018-01-05 00:25:44 +0530

    Extensions final commit

--- Extensions/Extensions.playground/Contents.swift
+++ Extensions/Extensions.playground/Contents.swift
@@ -3,14 +3,13 @@
 import UIKit
 
 func exampleOf(exampleName: String, completion: ()->()) {
+    print("\n")
     print("Example of \(exampleName)")
     print("-------------------------")
     completion()
-    print("\n")
 }
 
-exampleOf(exampleName: "Extensions") {
-}
+exampleOf(exampleName: "Extensions") {}
 class Player {
     let sports: String!
     init(sports: String) {
@@ -19,7 +18,9 @@ class Player {
 }
 
 //Extending Player class to perform Playing related activities
+print("I am extending Player")
 extension Player {
+    //Example of extensions including methods
     func play() {
         print("I am playing \(sports)")
     }
@@ -28,12 +29,11 @@ let ram = Player(sports: "Cricket")
 ram.play()
 
 
-//Extensions can add computed instance properties and computed type properties to existing types.
-exampleOf(exampleName: "Computed Properties") {
-}
+//MARK: Extensions can add computed instance properties and computed type properties to existing types.
+exampleOf(exampleName: "Extension adding Computed Properties") {}
 extension Date {
     
-    //Declare aa computed property on Date type to convert into string
+    //Declare as a computed property on Date type to convert into string
     var getString: String {
         let formatter = DateFormatter()
         formatter.dateFormat = "dd-mm-YYY"
@@ -42,3 +42,42 @@ extension Date {
     
 }
 print("Today's date in string format - \(Date().getString)") //Date() gets current date
+
+
+//MARK: Extensions can add new initializers to existing types.
+
+//Note - Extensions can add new convenience initializers to a class, but they cannot add new designated initializers or deinitializers to a class.
+exampleOf(exampleName: "Extension adding Initializers") {}
+
+class Customer {
+    var name: String!
+    var city: String!
+    var state: String!
+    var registrationDate = Date() //If we don't pass any value to registrationDate, it would be taken as today's date.
+    init(name: String, city: String, state: String) {
+        self.name = name
+        self.city = city
+        self.state = state
+    }
+}
+
+extension Customer {
+    //Declare a convinience initialiser
+    convenience init(regdDate: Date, attributes: [String: String]) {
+        if let name = attributes["name"], let city = attributes["city"], let state = attributes["state"] {
+            self.init(name: name, city: city, state: state)
+        } else {
+            self.init(name: "", city: "", state: "")
+        }
+        self.registrationDate = regdDate
+    }
+}
+
+let customerAttributes = ["name": "Johnny", "city": "Bhubaneswar", "state": "Odisha"]
+let customer1 = Customer(regdDate: Date(), attributes: customerAttributes)
+print("Customer \(customer1.name!) belongs from \(customer1.city!),\(customer1.state!).")
+
+
+//Extensions can be used to add subscripts to classes, structs or enums.
+//Extension can also be used to add nested types to classes, structs or enums.
+
